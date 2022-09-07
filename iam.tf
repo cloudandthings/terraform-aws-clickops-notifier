fb662e7e53a08a76f63893178ef291f4cffacebb
Paul Zietsman <phzietsman@gmail.com>
2022-09-07 18:22:52 +0200

    Fix tflint warning

--- iam.tf
+++ iam.tf
@@ -7,7 +7,7 @@ data "aws_iam_policy_document" "lambda_permissions" {
     ]
 
     resources = [
-      "${data.aws_s3_bucket.cloudtrail_bucket.arn}"
+      data.aws_s3_bucket.cloudtrail_bucket.arn
     ]
   }
 
