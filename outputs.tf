b68fb7f55c2394cb1052316215ee56b12214a867
Bjorn Olsen <bjorn@cloudandthings.io>
2023-02-22 11:20:37 +0200

    feat: For org deployments, use SNS to enable fan-out use cases.

--- outputs.tf
+++ outputs.tf
@@ -3,6 +3,11 @@ output "clickops_notifier_lambda" {
   value       = module.clickops_notifier_lambda
 }
 
+output "sns_topic" {
+  description = "Expose the bucket notification SNS details"
+  value       = aws_sns_topic.bucket_notifications
+}
+
 output "sqs_queue" {
   description = "Expose the bucket notification SQS details"
   value       = aws_sqs_queue.bucket_notifications
--- variables.tf
+++ variables.tf
@@ -185,27 +185,6 @@ variable "additional_iam_policy_statements" {
 }
 
 # Other configuration
-variable "additional_s3_bucket_notification_queues" {
-  description = "Additional SQS queues to configure as targets on the `aws_s3_bucket_notification` resource. See `variables.tf` for more information."
-  type = map(object({
-    # -- More information --
-    #
-    # Ref: https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/s3_bucket_notification#queue
-    #
-    # Required fields:
-    #
-    queue_arn = string
-    #
-    # Optional fields:
-    #
-    #   events = list(string)
-    #       default value: ["s3:ObjectCreated:*"]
-    #
-    #   filter_suffix = string
-    #       default value: ".json.gz"
-  }))
-  default = {}
-}
 variable "firehose_delivery_stream_name" {
   description = "Kinesis Firehose delivery stream name to output ClickOps events to."
   type        = string
